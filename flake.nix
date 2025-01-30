57a1a0e073ddcf0c31395e52bb8e60bfe59e0cb6
Sean Behan <codebam@riseup.net>
2025-01-30 06:32:49 -0500

    nixos flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+            cargo
+            vulkan-loader
+        ];
+      LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:${pkgs.vulkan-loader}/lib:${pkgs.vulkan-validation-layers}/lib";
+      VULKAN_SDK = "${pkgs.vulkan-headers}";
+      VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+      };
+    }
+  );
+}
